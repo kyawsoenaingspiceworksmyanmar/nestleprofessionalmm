a840c41863c7834f41759ea93d70107bb57e1bbf
nilarhtay <nilar.htay@spiceworks.co.jp>
2022-07-18 15:59:25 +0630

    recipe list col 02
    recipe list col 02

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,15 @@ $(document).ready(function(){
 		}
 	});
 });
+
+// Recipe Material
+$(document).ready(function(){
+	$(".recipe_material_sec .btn_click_detail").click(function(){
+		if($(".recipe_material_sec").hasClass("show_detail")){
+			$(".recipe_material_sec").removeClass("show_detail");
+		}
+		else{
+			$(".recipe_material_sec").addClass("show_detail");
+		}
+	});
+});
